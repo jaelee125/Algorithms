e7615962ea8172d9b5587fb2981239fb28e20a62
Lee, Jae <jae.lee01@sap.com>
2015-09-14 01:59:40 -0500

    Finished IsNumber

--- Is Number/src/IsNumber.java
+++ Is Number/src/IsNumber.java
@@ -1,4 +1,33 @@
-
 public class IsNumber {
 
+	public boolean isNumber(String string){
+		string = string.trim();
+		boolean isDecimal = false;
+		
+		if(string == null || string.isEmpty()){
+			return false;
+		}
+		
+		for(int i = 0; i<string.length(); i++){
+			if(string.charAt(i) == '-'){
+				if(i != 0){
+					return false;
+				}
+				continue;
+			}
+			else if(string.charAt(i) == '.'){
+				if(isDecimal || i == string.length()-1){
+					return false;
+				}
+				else {
+					isDecimal = true;
+				}
+			} else if(string.charAt(i) < '0' || string.charAt(i) > '9'){
+				return false;
+			}
+		}
+		return true;
+	}
 }
+
+
--- Is Number/test/IsNumberTest.java
+++ Is Number/test/IsNumberTest.java
@@ -1,4 +1,23 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 
 public class IsNumberTest {
+	IsNumber isNumber = new IsNumber();
+	
+	@Test
+	public void test() {
+		assertEquals(true, isNumber.isNumber("0.0"));
+		assertEquals(true, isNumber.isNumber("-1"));
+		assertEquals(true, isNumber.isNumber("-62.53"));
+		assertEquals(true, isNumber.isNumber("62.53"));
+		assertEquals(false, isNumber.isNumber("-62.5-3"));
+		assertEquals(false, isNumber.isNumber("-62.5.3"));
+		assertEquals(true, isNumber.isNumber(".0023"));
+		assertEquals(false, isNumber.isNumber("14123."));
+		assertEquals(false, isNumber.isNumber(".0.023"));
+		assertEquals(true, isNumber.isNumber("23"));
+	}
 
 }
